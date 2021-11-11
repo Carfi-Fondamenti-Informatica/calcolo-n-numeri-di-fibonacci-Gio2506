a6212bcd18324ca99b3f1f8fb92265c7bd435b11
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-11-11 10:57:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0,i=0,N1=1,N2=1,N3=0;
+    cin >> n;
+        if(n<2) {
+            cout << "errore" << endl;
+        }else{
+            cout << N1 << endl;
+            cout << N2<<endl;
+
+            for(int i=2;i<n;i++) {
+                N3 = N2 + N1;
+                N1 = N2;
+                N2 = N3;
+                cout << N3 << endl;
+            }
+        }
+    return 0;
 }
+
